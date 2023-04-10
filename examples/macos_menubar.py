d213239f63e0490fe0c7814e1ab583452d4b5eba
Mia yun Ruse <mia@yunru.se>
2023-04-10 02:09:57 +0100

    Add examples/macos_menubar.py
    I didn't even make it for this! But Sidra here is on 52% capacity, so it's good to know how much time I have til full (or empty).

---
+++ examples/macos_menubar.py
@@ -0,0 +1,36 @@
+"Display battery status in the macOS menubar."
+
+from battery import percent, is_charging, is_discharging, minutes_to_empty, minutes_to_full, capacity, design_capacity
+import rumps
+
+def timestring():
+    time: None | int = None
+    status = ""
+    if is_charging():
+        status = "⚡️"
+        time = minutes_to_full()
+    elif is_discharging():
+        time = minutes_to_empty()
+
+    timestring = ''
+    if time:
+        h, m = divmod(time, 60)
+        timestring = f'{h}h{m:02d}m'.removeprefix('0h')
+
+    return " ".join((
+        f"{percent()}%",
+        status,
+        timestring,
+    )).strip()
+
+class BatteryStatus(rumps.App):
+    @rumps.timer(10)
+    def update_menu(self, _):
+        self.title = timestring()
+
+    @rumps.timer(60 * 60)
+    def update_capacity(self, _):
+        self.quit_button.title = f"Capacity: {capacity() / design_capacity()*100:4.1f}%"
+
+if __name__ == "__main__":
+    BatteryStatus("BatteryStatus", "??% ?:??").run()
